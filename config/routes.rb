af2cabaad2a15b629c10d0be4989c8477fecbb2e
Jess Pesale <jess_pesale@aol.com>
2021-05-21 18:19:48 -0400

    added sessions controller/ routes for signup & login

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,17 @@
 Rails.application.routes.draw do
+
+  root to: "sessions#welcome"
+  get '/login' to: "sessions#new"
+  post 'login' to: "sessions#create"
+  get "/signup" to: "users#new"
+   #the resourse :users gives us the post users for users create
+
   resources :reviews
   resources :restaurants
   resources :users
+
+
+
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
