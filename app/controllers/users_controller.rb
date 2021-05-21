2fd27dc131c397ea9fcc26ababe8f48b21c7c50b
Jess Pesale <jess_pesale@aol.com>
2021-05-21 19:00:43 -0400

    finished sign up action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,17 +5,22 @@ class UsersController < ApplicationController
     end
 
     def create
-        @user = User.create(user_params)
-        if @user.valid?
-            session[:user_id] = @user.in    #log them in
+        @user = User.new(user_params)
+        if @user.save
+            session[:user_id] = @user.id    #log them in
             redirect_to user_path(@user)
         else
             render :new
         end
     end
 
+    def show
+        @user = User.find_by_id(params[:id])
+        # redirect_to users_path if !@user
+    end
 
-    privat
+
+    private
 
     def user_params
         params.require(:user).permit(:name, :email, :password)
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,12 @@
 class User < ApplicationRecord
     has_many :reviews
     has_many :restaurants, through: :reviews
+
+    validates :email, uniqueness: true, presence: true
+
+    has_secure_password
+        # gives us .authenticate method
+        # says a validation for password is true
+        # validates for password confirmation if there is one (can send in as a params)
+
 end
