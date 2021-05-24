36e6cde1c6fb77f29ae6781f3e5b706a6723909f
Jess Pesale <jess_pesale@aol.com>
2021-05-23 20:47:02 -0400

    added login function with flash message

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,20 @@ class SessionsController < ApplicationController
     end
 
     def new
- 
+        
+
+    end
+
+    def create #does the user exist in our system
+        # byebug
+        @user = User.find_by(email: params[:user][:email])
+            if @user && @user.authenticate(params[:user][:password ])
+                session[:user_id] = @user.id
+                redirect_to user_path(@user)
+            else
+                flash[:error] = "Sorry, something went wrong. Please try again. "
+                redirect_to login_path
+            end
     end
 
     def destroy
