498e803230a8206e3e8c49b1508baa4a8a543a5a
Jess Pesale <jess_pesale@aol.com>
2021-05-24 17:36:19 -0400

    added current_user helper method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+
+    private
+
+    def current_user
+        @current_user ||= User.find_by_id(session[:user_id])
+        # OR equals bc it will only call the db once if you 
+        # are using current user more than once in a method
+    end
+
+
+
 end
