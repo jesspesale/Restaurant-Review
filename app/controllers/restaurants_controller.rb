e29120ec3d63962a9746e608284cb567ae3d58ec
Jess Pesale <jess_pesale@aol.com>
2021-05-23 21:41:11 -0400

    started adding form for new restaurant

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,2 +1,17 @@
 class RestaurantsController < ApplicationController
+
+    def new
+        @restaurant = Restaurant.new
+    end
+
+    def create
+        @restaurant = Restaurant.new(restaurant_params)
+    end
+
+
+    private
+
+    def restaurant_params
+        params.require(:restaurant).permit(:restaurant_name, :address, :city, :state, :category)
+    end
 end
