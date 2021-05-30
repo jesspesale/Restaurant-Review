c83c17440a33e3c1dfef7b71043e499d6b35bbb6
Jess Pesale <jess_pesale@aol.com>
2021-05-30 16:04:23 -0400

    fixing up some things

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,6 +15,7 @@ class UsersController < ApplicationController
     end
 
     def show
+        # byebug
         @user = User.find_by_id(params[:id])
         @restaurants = Restaurant.all
         # redirect_to users_path if !@user
