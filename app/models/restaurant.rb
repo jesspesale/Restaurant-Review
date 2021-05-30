ddacbcad5179fd5da6aa0786c18fcf4ee8ebf2b8
Jess Pesale <jess_pesale@aol.com>
2021-05-30 15:22:04 -0400

    added new method to find or create a cuisine

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,9 +1,19 @@
 class Restaurant < ApplicationRecord
     has_many :reviews
     belongs_to :cuisine #gives .cuisine and .cuisine= methods
-    accepts_nested_attributes_for :cuisine
+    # accepts_nested_attributes_for :cuisine
     #allows you to save the new cuisine in the db through restaurants
+    # creates a cuisine_attributes=(attr) method
     has_many :users, through: :reviews
 
     validates :restaurant_name, presence: true
+
+    def cuisine_attributes=(attr)
+        # byebug
+        #find or create a cuisine with the name thats passed in
+        self.cuisine = Cuisine.find_or_create_by(name: attr[:name])
+        # associate the cuisine with the restaurant
+    end
+
+
 end
