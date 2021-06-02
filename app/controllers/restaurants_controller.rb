f5b1d7f3276b8bd89e864e4494cb0ac4bdb1de94
Jess Pesale <jess_pesale@aol.com>
2021-06-02 16:34:33 -0400

    more work on the edit function and links

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -7,37 +7,43 @@ class RestaurantsController < ApplicationController
 
     def index
         @restaurants = Restaurant.all
-        #  byebug
+        #   byebug
         # why do we have to do .users here not .user
     end
 
     def show
-        @user = current_user.id
+        # @user = current_user.id
     end
 
     def new
         @restaurant = Restaurant.new
-        # @restaurant = current_user.restaurants.build
         @restaurant.build_cuisine #instantiates a cuisine so we have one in the new restaurant form
     end
 
     def create
-         @restaurant = Restaurant.new(restaurant_params)
-        # @restaurant = current_user.restaurants.build(restaurant_params)
-        # @restaurant.user_id = session[:user_id]     #do I need this?
+        @restaurant = Restaurant.new(restaurant_params)
+        # @restaurant = Restaurant.find_or_initialize_by(restaurant_name: params[:restaurant_name]) do |restaurant|
+            # restaurant.assign_attributes(restaurant_params)
+        # end
+        # @restaurant.cuisine_id = params[:cuisine_id]
+        @restaurant.user_id = session[:user_id]     
+        # @restaurant = @current_user.restaurants.build(restaurant_params)
         if @restaurant.save #where validations happen
             redirect_to restaurant_path(@restaurant)
+
         else
             render :new
         end
     end
 
     def edit
-
+        #already finds the restaurant
+        @restaurant.build_cuisine
     end
 
     def update
         if @restaurant.update(restaurant_params)
+            @restaurant.build_cuisine
             redirect_to restaurant_path(@restaurant)
         else
             render :edit
@@ -53,7 +59,7 @@ class RestaurantsController < ApplicationController
     private
 
     def restaurant_params
-        params.require(:restaurant).permit(:restaurant_name, :address, :city, :state, cuisine_attributes: [:name])
+        params.require(:restaurant).permit(:restaurant_name, :address, :city, :state, :user_id, :cuisine_id, cuisine_attributes: [:name])
     end
 
     def find_restaurant
--- app/models/cuisine.rb
+++ app/models/cuisine.rb
@@ -1,4 +1,5 @@
 class Cuisine < ApplicationRecord
+    
     has_many :restaurants, dependent: :destroy
 
 end
